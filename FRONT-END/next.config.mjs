323fe37352c80a47a039ca4ecb4a0dcc29e6a513
STS <santiagowxer33sts@icloud.com>
2024-06-19 16:34:25 -0500

    rutas1

--- FRONT-END/next.config.mjs
+++ FRONT-END/next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     trailingSlash: true,
-    async rewrites() {
-      return [
-        {
-          source: '/:path*',
-          destination: '/:path*/',
-        },
-      ];
-    },
   };
   
   export default nextConfig;
